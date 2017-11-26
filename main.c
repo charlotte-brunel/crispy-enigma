c8def11ace6158dd346bb95584ca0dec056c9613
jojoh2943 <johannazoppi@hotmail.fr>
2017-11-26 18:13:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -17,16 +17,17 @@
 int main()
 {
 
+//int convergence=0; //convergence = 0 tant que T(p_mot_selected) est différent de T'(p_mot_selected_prim) et est égal à 1 quand la convergence est atteinte
 FILE *file_info=NULL; //fichier contenant les infos sur les s�quences
 int longueur_masque = 5;
-//int pos_max;
+int pos_max;
+double distance_PSSM; //distance entre deux matrice PSSM
 int position= 0;
-//int score_max;
+double score_max;
 int cpt_mot;
 int n_sequence;
-//float score;
-char mot[longueur_masque+1];
-int masque[longueur_masque];
+char Ct[longueur_masque];
+int masque[longueur_masque]; //motif consensus pour raffiner la PSSM
 memset(masque, 0, sizeof masque);
 //int* p_masque= masque;
 int nb_fenetre=2;
@@ -34,7 +35,7 @@ TPtr_Cellkmer element_kmer=malloc(sizeof(TCellkmer));
 TPtr_Cellkmer tete_liste_kmer= element_kmer;
 TPtr_Cellkmer tete_liste_kmer2= element_kmer;
 TPtr_Cellkmer tete_liste_kmer3= element_kmer;
-TPtr_Cellkmer tete_liste_kmer4= element_kmer;
+//TPtr_Cellkmer tete_liste_kmer4= element_kmer;
 element_kmer->suiv_kmer=NULL;
 TPtr_CellSequence element_sequence= malloc(sizeof(TCellSequence));
 TPtr_CellSequence tete_liste_sequence= element_sequence;
@@ -54,13 +55,98 @@ TPtr_Cell_Motif_PSSM element_liste_motif_PSSM= malloc(sizeof(TCell_Motif_PSSM));
 TPtr_Cell_Motif_PSSM tete_liste_motif_PSSM= element_liste_motif_PSSM;
 TPtr_Cell_Motif_PSSM tete_liste_motif_PSSM2= element_liste_motif_PSSM;
 TPtr_Cell_Motif_PSSM tete_liste_motif_PSSM_pour_calcul= element_liste_motif_PSSM;
+TPtr_Cell_Motif_PSSM tete_mot_selected= malloc(sizeof(TCell_Motif_PSSM)); // on créé une nouvelle tete pour recuperer les mots pour ameliorer la PSSM
+TPtr_Cell_Motif_PSSM p_mot_selected= tete_mot_selected;
+
+
+TPtr_Cell_Motif_PSSM tete_mot_selected_calcul_PSSM= tete_mot_selected;
 element_liste_motif_PSSM->suiv_motif=NULL;
 
-TPtr_Mot_Ameliorer_PSSM element_mot= (TMot_Ameliorer_PSSM*)malloc(sizeof(TMot_Ameliorer_PSSM));
-TPtr_Mot_Ameliorer_PSSM p_mot= element_mot;
+TPtr_Mot_Ameliorer_PSSM tete_mot= (TMot_Ameliorer_PSSM*)malloc(sizeof(TMot_Ameliorer_PSSM));
+TPtr_Mot_Ameliorer_PSSM p_mot= tete_mot;
+TPtr_Mot_Ameliorer_PSSM tete_mot_pour_st2_prim= (TMot_Ameliorer_PSSM*)malloc(sizeof(TMot_Ameliorer_PSSM));
+
+
+//VARIABLES SCORE ST1 ST2:
+Ptr_st1 tete_st1=malloc(sizeof(st1));
+Ptr_st1 p_st1=tete_st1;
+
+Ptr_st2 tete_st2=malloc(sizeof(st2));
+Ptr_st2 p_st2=tete_st2;
+
+Ptr_st2 tete_st2_prim=malloc(sizeof(st2));
+Ptr_st2 p_st2_prim=tete_st2_prim;
 
 //MATRICE PSSM ET MOTIF CONSENSUS:
-double** matrice_PSSM[4][6]; 
+double** matrice_PSSM;
+
+/****
+ * MATRICE PSSM
+ ****/
+ int nb_ligne;
+ int taille_motif=5;
+
+    matrice_PSSM= malloc(4 * sizeof(*matrice_PSSM));
+    /*if (matrice_PSSM==NULL)
+    {
+        free(matrice_PSSM);
+    }*/
+
+    for(nb_ligne=0 ; nb_ligne < 4 ; nb_ligne++)
+    {
+        matrice_PSSM[nb_ligne] = malloc(taille_motif * sizeof(*(matrice_PSSM[nb_ligne]))); //On alloue des tableaux de 'taille2' variables.
+        /*if(matrice_PSSM[nb_ligne] == NULL)
+        {
+            for(nb_ligne=0 ; nb_ligne < 4 ; nb_ligne++)
+            {
+                free(matrice_PSSM[nb_ligne]);
+            }
+        }*/
+    }
+
+int cpt,k ;
+//initialisation de la matrice � 0:
+    for (cpt=0; cpt<4; cpt++)
+    {
+        for(k=0; k<taille_motif; k++)
+        {
+            (matrice_PSSM)[cpt][k]= 0;
+        }
+    }
+
+
+double** matrice_PSSM_nouv;
+
+/****
+ * MATRICE PSSM NOUV
+ ****/
+
+    matrice_PSSM_nouv= malloc(4 * sizeof(*matrice_PSSM_nouv));
+    /*if (matrice_PSSM_nouv==NULL)
+    {
+        free(matrice_PSSM_nouv);
+    }*/
+
+    for(nb_ligne=0 ; nb_ligne < 4 ; nb_ligne++)
+    {
+        matrice_PSSM_nouv[nb_ligne] = malloc(taille_motif * sizeof(*(matrice_PSSM_nouv[nb_ligne]))); //On alloue des tableaux de 'taille2' variables.
+      /*  if(matrice_PSSM_nouv[nb_ligne] == NULL)
+        {
+            for(nb_ligne=0 ; nb_ligne < 4 ; nb_ligne++)
+            {
+                free(matrice_PSSM_nouv[nb_ligne]);
+            }
+        }*/
+    }
+//initialisation de la matrice � 0:
+    for (cpt=0; cpt<4; cpt++)
+    {
+        for(k=0; k<taille_motif; k++)
+        {
+            (matrice_PSSM_nouv)[cpt][k]= 0;
+        }
+    }
+
 
 
 
@@ -70,15 +156,16 @@ double** matrice_PSSM[4][6];
 //D�claration des variables
 srand(time(0));
 
-int nb_sequence= 10; // nombre de s�quence � g�n�rer
+double nb_sequence= 10; // nombre de s�quence � g�n�rer
 int i,j; //it�rateur de boucle
 char nucleo= ' ';
 
-ptr_struct_seq element_generation_sequence=malloc(sizeof(structure_sequence)); // On cr�� le premier �l�ment de la structure
-ptr_struct_seq tete_liste_pour_parcours_masque= element_generation_sequence;
-ptr_struct_seq p_generation_seq= element_generation_sequence;
-ptr_struct_seq tete_liste_pour_recup_motif= element_generation_sequence;
-ptr_struct_seq tete_liste_pour_insertion_motif= element_generation_sequence;
+ptr_struct_seq tete_generation_sequence=malloc(sizeof(structure_sequence)); // On cr�� le premier �l�ment de la structure
+ptr_struct_seq element_generation_sequence= tete_generation_sequence;
+ptr_struct_seq tete_liste_pour_parcours_masque= tete_generation_sequence;
+ptr_struct_seq tete_liste_pour_recup_motif= tete_generation_sequence;
+ptr_struct_seq tete_liste_pour_insertion_motif= tete_generation_sequence;
+ptr_struct_seq p_generation_seq= tete_generation_sequence;
 ptr_struct_seq element_generation_sequence_next= NULL;
 ptr_liste_motif element_motif= malloc(sizeof(liste_chaine_motif));
 ptr_liste_motif tete_liste_motif= element_motif;
@@ -115,57 +202,103 @@ kmer_present_dans_chaque_sequence(nb_sequence, &tete_liste_kmer2, &tete_liste_se
 affichage_dictionnaire_kmer(&tete_liste_kmer3, &tete_liste_sequence3, &tete_liste_pos3);
 affichage_motif_selectionne(&tete_liste_kmer_selectionne2, &tete_liste_motif_PSSM2);
 // On calculera la PSSM seulement pour les kmers qui sont présent dans plus de 7 sequences:
-while (tete_liste_kmer4 != NULL)
-{
-	if (tete_liste_kmer4->nb_sequence>=7)
-	{	
+//while (tete_liste_kmer4 != NULL)
+//{
+	//if (tete_liste_kmer4->nb_sequence>=7)
+	//{
 		calcul_PSSM(&tete_liste_kmer_selectionne_pour_calcul, &tete_liste_motif_PSSM_pour_calcul, &file_info, &matrice_PSSM);
-		while (p_generation_seq != NULL) //pour chaque sequence
+
+		do //repeter l'amélioration de la PSSM jusqu'à convergence
 		{
-			//pos_max= -1;
-			//score_max= -100;
-			//pour chaque mot:
-			while (position<30)
+			while (p_generation_seq != NULL) //pour chaque sequence
 			{
-				n_sequence= p_generation_seq->numero_sequence;
-				printf("numero seq: %d \n", n_sequence);
-				for (cpt_mot=0; cpt_mot<=longueur_masque; cpt_mot++)
+				pos_max= -1;
+				score_max= -100;
+				//pour chaque mot:
+				while (position<=25)
 				{
-					mot[cpt_mot]=p_generation_seq-> sequence[position];
-					p_mot->mot[cpt_mot]= p_generation_seq-> sequence[position];
-					position++; 
-					if (cpt_mot==longueur_masque)
+					n_sequence= p_generation_seq->numero_sequence;
+					for (cpt_mot=0; cpt_mot<=longueur_masque; cpt_mot++)
 					{
-						mot[longueur_masque]= '\0';
-						p_mot->mot[longueur_masque]= '\0';
-						printf("%s \n", mot);
-						printf("%s \n", p_mot->mot);
-						printf("PSSM: %f \n", matrice_PSSM[0][0]);
-						printf("longueur masque: %d \n", longueur_masque);
-						calcul_score(&p_mot, &matrice_PSSM, n_sequence, &p_generation_seq, longueur_masque);
-					} 
-					printf("%d \n", position);
-					printf("cpt mot: %d \n", cpt_mot);
-					printf("longueur_masque: %d \n", longueur_masque);
-					
+						p_mot->mot[cpt_mot]= p_generation_seq-> sequence[position];
+						if (cpt_mot==longueur_masque)
+						{
+							p_mot->mot[longueur_masque]= '\0';
+							calcul_score(&p_mot, &matrice_PSSM, n_sequence, &p_generation_seq, longueur_masque);
+						}
+						position++;
+					}
+					position= position -5;
+					//si le score de ce mot est supérieur au score max
+					if (p_mot->score_mot> score_max)
+					{
+						score_max= p_mot-> score_mot;
+						pos_max= position;
+						strcpy(p_mot_selected->motif, p_mot->mot);
+					}
+					//p_mot->seq=n_sequence;
+					TPtr_Mot_Ameliorer_PSSM p_nouv_mot=malloc(sizeof(TMot_Ameliorer_PSSM));
+					p_mot->next_mot= p_nouv_mot;
+					p_mot=p_nouv_mot;
 				}
-				printf("fonction ???");
-				printf("FONCTION !!!");
-				TPtr_Mot_Ameliorer_PSSM p_nouv_mot= malloc(sizeof(TMot_Ameliorer_PSSM));
-				p_mot->next_mot= p_nouv_mot;
-				p_mot=p_nouv_mot;
+				//Post condition: le mot de la sequence courante le plus proche de la PSSM est identifié !
+				position=0;
+				TPtr_Cell_Motif_PSSM p_mot_selected_suiv= malloc(sizeof(TCell_Motif_PSSM));
+				p_mot_selected-> suiv_motif= p_mot_selected_suiv;
+				p_mot_selected=p_mot_selected_suiv;
+				score_max=-100;
+				pos_max=0;
+				p_generation_seq= p_generation_seq-> next_sequence;
+
 			}
-			p_generation_seq= p_generation_seq-> next_sequence;
-		}
-	}
-	tete_liste_kmer4= tete_liste_kmer4->suiv_kmer;
-}
+			calcul_nouvelle_PSSM(&tete_mot_selected_calcul_PSSM, &matrice_PSSM_nouv, nb_sequence, &Ct);
+			printf("Ct: %s \n", Ct);
+			distance_PSSM=dist_PSSM(&matrice_PSSM, &matrice_PSSM_nouv, &distance_PSSM);
 
 
+			if (distance_PSSM>0.8)
+			{
+				for (cpt=0; cpt<4; cpt++) //ancienne_matrice= nouv_matrice.
+    				{
+        				for(k=0; k<taille_motif; k++)
+        				{
+            					(matrice_PSSM)[cpt][k]= (matrice_PSSM_nouv)[cpt][k];
+        				}
+    				}
+				for (cpt=0; cpt<4; cpt++) //reinitialisation de matrice_PSSM_nouv à 0.
+    				{
+        				for(k=0; k<taille_motif; k++)
+        				{
+            					(matrice_PSSM_nouv)[cpt][k]= 0;
+        				}
+    				}
+				p_generation_seq=tete_generation_sequence;
+				p_mot_selected=tete_mot_selected;
+				p_mot=tete_mot;
+			}
+		}while(distance_PSSM>0.8);
+		//Post-condition: tous les mots de longueur l maximisant le score sont contenu dans la structure chainée p_mot_selected.
+		//Le motif consensus de cette structure a été calculé
+		//Il faut maintenant calculer la distance de Hamming entre le motif consensus et tous les mots de longueur l contenu dans la structure p_mot
+		p_mot_selected= tete_mot_selected;
+		//RAFFINER - Version 1:
+		distanceHammingSt1(&Ct, &p_mot_selected, &p_st1);
+		// RAFFINER - Version 2:
+		p_mot_selected= tete_mot_selected;
+		//do{
+			distanceHammingSt2(&Ct, &p_mot_selected, &p_st2);
+			//T' (p_mot_selected_prim) <- mot mi de longueur l de Si, mi minimisant Dh(mi, Ct)
+			TPtr_Mot_Ameliorer_PSSM p_mot_st2_prim=tete_mot_pour_st2_prim;
+			p_generation_seq=tete_generation_sequence;
+			distanceHammingSt2_prim(&Ct, &p_generation_seq, &p_mot_st2_prim, &p_st2_prim);
+
+		//}while(convergence == 0);
 
 
-//printf("\npleaase: %s", p_motif_consensus->motif_consensus);
-//calcul_ST1()
+	//}
+	//tete_liste_kmer4= tete_liste_kmer4->suiv_kmer;
+//}
+
 
 
 
